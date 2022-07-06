1ca280d371bcd11200147e783d7061437f57402c
PikaPika0710 <102190298@sv1.dut.udn.vn>
2022-07-07 01:24:13 +0700

    edit base URL

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import axios from "axios"
 import store from './store'
 
-axios.defaults.baseURL = "http://localhost:8000"
+// axios.defaults.baseURL = "http://localhost:8000"
 
+axios.defaults.baseURL = "https://vietdev-shop.herokuapp.com/"
 createApp(App).use(store).use(router, axios).mount('#app')
